34a697766b36eeb50689b2dd3b5ab557cc0217cf
DaryaYa <lyalyaka@mail.ru>
2019-11-04 00:23:29 +0300

    fix

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ body.append(keyboard);
 let butCollection = keyboard.querySelectorAll('.buttons');
 
 
-let keysEng = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i','o','p', '[', ']', '/', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Enter', '', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Sft', '←', '↑', 'Ctrl', 'Fn', 'Alt', 'Space', 'Fn', 'Win', 'Print', 'Ctrl', '↓', '→'];
+let keysEng = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i','o','p', '[', ']', '/', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Enter', '', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '/', 'Sft', '←', '↑', 'Ctrl', 'Fn', 'Alt', 'Space', 'Fn', 'Win', 'Print', 'Ctrl', '↓', '→'];
 
 let keysRus = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', '', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Sft', '←', '↑', 'Ctrl', 'Fn', 'Alt', 'Space', 'Fn', 'Win', 'Print', 'Ctrl', '↓', '→'];
 
@@ -61,11 +61,11 @@ const typing = event => {
 
 
     if(target.classList == 'buttons') {
-    if(target.textContent == 'Backspace') {
+    if(target.textContent.toLowerCase() == 'backspace') {
         textarea.value = textarea.value.slice(0, -1);
-    }   else if(target.textContent == 'Tab' || target.textContent == 'Caps Lock' || target.textContent == 'Shift' ||                  target.textContent == 'Ctrl' || target.textContent == 'Enter' || target.textContent == 'Alt' ||                       target.textContent == 'Fn' || target.textContent == 'Win' || target.textContent == 'Sft' ||                           target.textContent == 'Print') {
+    }   else if(target.textContent.toLowerCase() == 'tab' || target.textContent.toLowerCase() == 'caps lock' || target.textContent.toLowerCase() == 'shift' || target.textContent.toLowerCase() == 'ctrl' || target.textContent.toLowerCase() == 'enter' || target.textContent.toLowerCase() == 'alt' ||                       target.textContent.toLowerCase() == 'fn' || target.textContent.toLowerCase() == 'win' || target.textContent.toLowerCase() == 'sft' ||                           target.textContent.toLowerCase() == 'print') {
                        textarea.value = textarea.value;
-    }   else if(target.textContent == 'Space') {
+    }   else if(target.textContent.toLowerCase() == 'space') {
                        textarea.value += ' ';
     }
         else  {
@@ -110,11 +110,11 @@ const realKey = (event) => {
         butCollection.forEach(buttons => buttons.classList.remove('selected'));
 
     for(let i = 0; i < keysEng.length; i++) {
-    if (event.key == keysEng[i])  {
+    if (event.key == keysEng[i] || event.key == engUpp[i])  {
         butCollection[i].classList.add('selected1');
-    } else if (event.key == keysRus[i]) {
+    } else if (event.key == keysRus[i] || event.key == rusUpp[i]) {
         butCollection[i].classList.add('selected1');
-    }
+    } 
     }  
 }
 
